bbbd2d6590f5db673431a214cbf44972f9c7444d
ivanchor85 <56176529+ivanchor85@users.noreply.github.com>
2019-10-10 15:09:36 -0700

    Raindow Random

--- AquaSimApplication.java
+++ AquaSimApplication.java
@@ -21,27 +21,39 @@ public class AquaSimApplication
      *  This is the main function.  It executes the program.
      *  @param    String args[] is never used
      **/
+    private static Random generator;
+    
     public static void main(String args[])
     {
         System.out.println("Aquarium Simulation starts in: 5 Minutes");
 
         // CONSTRUCT OBJECTS NEEDED FOR THE AQUARIUM SIMULATION.
-        Random generator = new Random();
-        // int randNum = generator.nextInt(10);
+            generator = new Random();
+           
         
         // Construct the aquarium.  Specify its dimensions when creating it.
         Aquarium aqua;                 // create reference to an Aquarium ...
-        aqua = new Aquarium(500, 1000); // ... object that has now been created
+        aqua = new Aquarium(700, 700); // ... object that has now been created
      
         // Construct fish and add them to the aquarium.
-        AquaFish Bee = new AquaFish(aqua, Color.RED);
+        
+        AquaFish Bee = new AquaFish(aqua, getColor());
         aqua.add(Bee);
         
-        AquaFish Joe = new AquaFish(aqua, Color.BLUE);
+        AquaFish Joe = new AquaFish(aqua, getColor());
         aqua.add(Joe);
         
-        AquaFish Tree = new AquaFish(aqua, Color.BLUE);
+        AquaFish Tree = new AquaFish(aqua, getColor());
         aqua.add(Tree);
+        
+        AquaFish Aye = new AquaFish(aqua, getColor());
+        aqua.add(Aye);
+        
+        AquaFish See = new AquaFish(aqua, getColor());
+        aqua.add(See);
+        
+        AquaFish Deed = new AquaFish(aqua, getColor());
+        aqua.add(Deed);
         
         
         
@@ -68,17 +80,42 @@ public class AquaSimApplication
         // Make the fish move and redisplay.
         for(int i=0;i<20;i--){
         Bee.moveForward();
-        if(Bee.atWall()){
-            Bee.changeDir();}
+        
+        if(Bee.atWall())
+        {
+            Bee.changeDir();
+        }
             
         Joe.moveForward();
-        if(Joe.atWall()){
-            Joe.changeDir();}
+        if(Joe.atWall())
+        {
+            Joe.changeDir();
+        }
             
         Tree.moveForward();
-        if(Tree.atWall()){
-            Tree.changeDir();}
+        if(Tree.atWall())
+        {
+            Tree.changeDir();
+        }
 
+        Aye.moveForward();
+        if(Aye.atWall())
+        {
+            Aye.changeDir();
+        }
+            
+        See.moveForward();
+        if(See.atWall())
+        {
+            See.changeDir();
+        }
+            
+        Deed.moveForward();
+        if(Deed.atWall())
+        {
+            Deed.changeDir();
+        }
+        
         userInterface.showAquarium();}
         
         // WRAP UP.
@@ -88,4 +125,31 @@ public class AquaSimApplication
 
     }//end main
 
+    public static Color getColor(){
+        int randNum = generator.nextInt(6);
+        if(randNum==0)
+        {
+            return Color.RED;
+        }
+        else if(randNum==1)
+        {
+            return Color.BLUE;
+        }
+        else if (randNum==2)
+        {
+            return Color.GREEN;
+        }
+        else if (randNum==3)
+        {
+            return Color.ORANGE;
+        }
+        else if (randNum==4)
+        {
+            return Color.YELLOW;
+        }
+        else
+        {
+            return Color.MAGENTA;
+        }
+        }    
 }//end class
